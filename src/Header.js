fdef7e11840fde38770ce0841fe2f33383f0f81d
sabbirahmedseam <sabbirahmedseam1@gmail.com>
2022-10-25 09:47:55 +0600

    send

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
-
+import React from "react";
+import { Image } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import pic from "./images/edtech.png";
 
 const Header = () => {
-    return (
-        <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-    );
+  return (
+    <Navbar bg="primary" variant="dark">
+      <Image style={{ height: "30px" }} rounded src={pic}></Image>
+      <Navbar.Brand href="#home">Coding-tutorial</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Courses</Nav.Link>
+        <Nav.Link href="#features">FAQ</Nav.Link>
+        <Nav.Link href="#pricing">Blog</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
 };
 
 export default Header;
--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from './Header';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Main = () => {
-    return (
-        <div>
-           <Header></Header>
+  return (
+    <div>
+      <Header></Header>
+      <Container>
+        <Row>
+          <Col lg="3"></Col>
+          <Col lg="9">
             <Outlet></Outlet>
-        </div>
-    );
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Main;
