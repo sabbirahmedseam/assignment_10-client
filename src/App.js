6940a07b09dcd6a94ada41f87e54f94ff03d9dcc
sabbirahmedseam <sabbirahmedseam1@gmail.com>
2022-10-25 08:46:51 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { routes } from './routes';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
