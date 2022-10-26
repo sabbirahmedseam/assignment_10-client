68b285decfc8bee2a18fbb9adabc418bebd1c0e5
sabbirahmedseam <sabbirahmedseam1@gmail.com>
2022-10-26 15:54:21 +0600

    send

--- src/Error.js
+++ src/Error.js
@@ -5,7 +5,7 @@ const Error = () => {
     <div>
       <h1>404 not found</h1>
       <h3>Please Check Your Search Bar</h3>
-      <h5>Try Again</h5>
+      <h5>Try Again After</h5>
     </div>
   );
 };
